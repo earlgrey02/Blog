e940c9bb70cb81fc13a5e9d58774b7b2cbf32a0d
earlgrey02 <san06036@naver.com>
2023-12-05 19:12:59 +0900

    Feat: RootLayout에 Header 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import './global.css'
 import { ReactNode } from 'react'
 import Head from './head'
+import Header from '@/component/Header/Header'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
       <Head />
       <body>
+        <Header />
         {children}
       </body>
     </html>
