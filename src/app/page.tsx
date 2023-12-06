5a1365997ca14bd3d10168f4bcb002dec49aeb0b
earlgrey02 <san06036@naver.com>
2023-12-06 17:52:53 +0900

    Feat: RootPage에 있던 컴포넌트를 Intro 컴포넌트 내부로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import styles from './Page.module.css'
 import Intro from '@/component/Intro/Intro'
 import Board from '@/component/Board/Board'
 import Footer from '@/component/Footer/Footer'
-import MotionedDiv from '@/lib/motion'
 
 const Page = () => {
   return (
@@ -10,18 +9,6 @@ const Page = () => {
       <Intro />
       <Board />
       <Footer />
-      <MotionedDiv
-        className={styles.square}
-        initial={{
-          opacity: 0,
-          transform: 'rotate(0deg)'
-        }}
-        animate={{
-          opacity: 1,
-          transform: 'rotate(135deg)',
-          transition: { duration: 5, ease: [0, 0.8, 0.4, 1.0] }
-        }}
-      />
     </div>
   )
 }
