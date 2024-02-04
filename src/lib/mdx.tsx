fe05f969d863855ca31c2c9159db8ec01b18205f
earlgrey02 <san06036@naver.com>
2024-02-04 19:51:51 +0900

    Feat: mdx에서 img 태그를 커스터마이징한 Image 컴포넌트로 대체하도록 구현

--- src/lib/mdx.tsx
+++ src/lib/mdx.tsx
@@ -1,20 +1,12 @@
 import type { MDXComponents } from 'mdx/types'
-import Image from 'next/image'
 import Link from 'next/link'
-import styles from '@/app/post/[id]/Page.module.css'
+import Image from './image'
 
 const mdxComponents = (id: number): MDXComponents => ({
   p: ({ children }) => <span>{children}</span>,
   a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
   img: ({ src, alt }) => (
-    <div className={styles.image}>
-      <Image
-        src={`/posts/${id}/${src}`}
-        alt={alt as string}
-        layout="fill"
-        objectFit="contain"
-      />
-    </div>
+    <Image src={`/posts/${id}/${src}`} alt={alt as string} />
   )
 })
 
