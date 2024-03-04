cce9d25c892c48eadb047fc8800cd54ba2e2ad74
earlgrey02 <san06036@naver.com>
2024-03-05 03:15:08 +0900

    Feat: Pagination index 구현

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -5,9 +5,45 @@ import Post, { variants } from '@/component/Post/Post'
 import MotionedDiv from '@/lib/motion'
 
 const Page = () => {
-  const posts = allPosts.sort((post1, post2) =>
-    compareDesc(new Date(post1.date), new Date(post2.date))
+  const [page, setPage] = useState(0)
+  const [isEnter, setIsEnter] = useState(true)
+
+  const pages = useMemo(
+    () =>
+      allPosts
+        .sort((post1, post2) =>
+          compareDesc(new Date(post1.date), new Date(post2.date))
+        )
+        .reduce((pages, post, index) => {
+          const page = Math.floor(index / 4)
+
+          if (!pages[page]) pages[page] = []
+          pages[page].push(post)
+
+          return pages
+        }, [] as any[][]),
+    []
   )
+  const variants: Variants = useMemo(
+    () => ({
+      initial: { opacity: 0, y: 10, filter: 'blur(0.5px)' },
+      animate: {
+        opacity: 1,
+        y: 0,
+        filter: 'blur(0px)',
+        transition: {
+          duration: 1,
+          staggerChildren: 0.5,
+          delayChildren: isEnter ? 0.5 : 0
+        }
+      }
+    }),
+    [isEnter]
+  )
+
+  useEffect(() => {
+    setIsEnter(false)
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -32,6 +68,21 @@ const Page = () => {
           <Post post={post} key={post.id} />
         ))}
       </MotionedDiv>
+      <div className={styles.paginator}>
+        {pages.map((_, index) => (
+          <div
+            className={styles.index}
+            style={
+              index == page
+                ? { backgroundColor: 'rgb(210, 210, 210)', scale: 1.05 }
+                : {}
+            }
+            onClick={() => setPage(index)}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
