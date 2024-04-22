ab5559058fb656298a37f9569380dfcb31224df7
earlgrey02 <san06036@naver.com>
2024-04-22 23:58:49 +0900

    Feat: 페이지네이션을 MUI로 대체

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -7,6 +7,7 @@ import MotionedDiv from '@/lib/motion'
 import { useEffect, useMemo, useState } from 'react'
 import { Variants } from 'framer-motion'
 import { useDispatch, useSelector } from 'react-redux'
+import { Pagination } from '@mui/material'
 import { setPage } from '@/redux/reducers/pageSlice'
 
 const Page = () => {
@@ -17,19 +18,20 @@ const Page = () => {
   const pages = useMemo(
     () =>
       allPosts
-      .sort((post1, post2) =>
-        compareDesc(new Date(post1.date), new Date(post2.date))
-      )
-      .reduce((pages, post, index) => {
-        const page = Math.floor(index / 4)
+        .sort((post1, post2) =>
+          compareDesc(new Date(post1.date), new Date(post2.date))
+        )
+        .reduce((pages, post, index) => {
+          const page = Math.floor(index / 4)
 
-        if (!pages[page]) pages[page] = []
-        pages[page].push(post)
+          if (!pages[page]) pages[page] = []
+          pages[page].push(post)
 
-        return pages
-      }, [] as Post[][]),
+          return pages
+        }, [] as Post[][]),
     []
   )
+
   const variants: Variants = useMemo(
     () => ({
       initial: { opacity: 0, y: 10, filter: 'blur(0.5px)' },
@@ -39,7 +41,7 @@ const Page = () => {
         filter: 'blur(0px)',
         transition: {
           duration: 1,
-          staggerChildren: 0.5,
+          staggerChildren: 0.35,
           delayChildren: isEnter ? 0.5 : 0
         }
       }
@@ -66,28 +68,27 @@ const Page = () => {
       <MotionedDiv
         className={styles.posts}
         variants={variants}
-        initial='initial'
-        animate='animate'
+        initial="initial"
+        animate="animate"
         key={page}>
         {pages[page].map(post => (
           <Post post={post} variants={variants} key={post.id} />
         ))}
       </MotionedDiv>
-      <div className={styles.paginator}>
-        {pages.map((_, index) => (
-          <div
-            className={styles.index}
-            style={
-              index == page
-                ? { backgroundColor: 'rgb(210, 210, 210)', scale: 1.05 }
-                : {}
-            }
-            onClick={() => dispatch(setPage(index))}
-            key={index}>
-            {index + 1}
-          </div>
-        ))}
-      </div>
+      <MotionedDiv
+        className={styles.paginator}
+        initial={{ opacity: 0, filter: 'blur(0.8px)' }}
+        animate={{
+          opacity: 1,
+          filter: 'blur(0px)',
+          transition: { duration: 3 }
+        }}>
+        <Pagination
+          count={pages.length}
+          page={page + 1}
+          onChange={(_, index) => dispatch(setPage(index - 1))}
+        />
+      </MotionedDiv>
     </div>
   )
 }
