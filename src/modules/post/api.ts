98a0537add3c9d743408bf65962471deb77b1975
earlgrey02 <san06036@naver.com>
2024-05-29 20:15:04 +0900

    Feat: 모든 게시글을 날짜를 기준으로 정렬해서 가져오는 getPosts 함수 구현

--- src/modules/post/api.ts
+++ src/modules/post/api.ts
@@ -1,5 +1,11 @@
 import { allPosts } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+
+const getPosts = () =>
+  allPosts.sort((post1, post2) =>
+    compareDesc(new Date(post1.date), new Date(post2.date))
+  )
 
 const getPostById = (id: number) => allPosts.find(post => post.id == id)
 
-export { getPostById }
+export { getPosts, getPostById }
