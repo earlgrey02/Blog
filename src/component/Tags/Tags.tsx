f5280404c021a6938710a00a337039fc9d1034f4
earlgrey02 <san06036@naver.com>
2024-05-29 20:13:56 +0900

    Feat: 태그 리스트 UI 구현

---
+++ src/component/Tags/Tags.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { getPosts } from '@/modules/post/api'
+import { useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setTags } from '@/redux/reducers/filterSlice'
+import styles from './Tags.module.css'
+import { setIndex } from '@/redux/reducers/pageSlice'
+
+const Tags = () => {
+  const filter = useSelector((store: Store) => store.filter)
+  const dispatch = useDispatch()
+  const tags = useMemo(
+    () =>
+      Array.from(
+        new Set(
+          getPosts()
+            .map(tag => tag.tags)
+            .flat()
+        )
+      ),
+    []
+  )
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.tags}>
+        {tags.map(tag => {
+          const selected = filter.tags.includes(tag)
+
+          return (
+            <div
+              style={selected ? { backgroundColor: 'rgb(230, 230, 230)' } : {}}
+              className={styles.tag}
+              onClick={() => {
+                dispatch(
+                  setTags(
+                    selected
+                      ? filter.tags.filter(_tag => _tag !== tag)
+                      : [...filter.tags, tag]
+                  )
+                )
+                dispatch(setIndex(0))
+              }}>
+              {tag}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Tags
