00e5f7bece3f2daec96d6e46aed36ba7cf6af520
earlgrey02 <san06036@naver.com>
2024-06-11 21:37:40 +0900

    Feat: 코드 블록 폰트 변경

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,9 +5,11 @@ const Head = () => {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@2.0/nanumsquare.css"
       />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
+        href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/fira_code.css"
       />
       <meta name="robots" content="index, follow" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
