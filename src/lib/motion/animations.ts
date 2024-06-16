07be20fa1d4bbe307c87d02440bd099f1354d2a2
earlgrey02 <san06036@naver.com>
2024-06-16 17:15:57 +0900

    Feat: 애니메이션 관련 유틸리티 추가

---
+++ src/lib/motion/animations.ts
@@ -0,0 +1,41 @@
+'use client'
+import { Variants } from 'framer-motion'
+import { keyframes } from '@emotion/react'
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const fadeIn: Variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 }
+}
+
+const fadeInUp: Variants = {
+  initial: {
+    opacity: 0,
+    y: 10
+  },
+  animate: {
+    opacity: 1,
+    y: 0
+  }
+}
+
+const fadeInRight: Variants = {
+  initial: {
+    opacity: 0,
+    x: -10
+  },
+  animate: {
+    opacity: 1,
+    x: 0
+  }
+}
+
+export { spin, fadeIn, fadeInUp, fadeInRight }
