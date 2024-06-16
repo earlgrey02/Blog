bd6b2f894f407a4bbe7ee89afa2174584988248a
earlgrey02 <san06036@naver.com>
2024-06-16 17:14:10 +0900

    Refactor: ReduxProvider 디렉토리 변경

--- src/redux/ReduxProvider.tsx
+++ src/redux/ReduxProvider.tsx
@@ -1,14 +1,14 @@
 'use client'
+import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
-import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
 }
 
 const ReduxProvider = ({ children }: Props) => {
-  return <Provider store={store}>{children}</Provider>
+  return <Provider store={store} children={children} />
 }
 
 export default ReduxProvider
