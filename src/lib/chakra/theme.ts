c02cf67d032016d3f14de33ea06c8e971377d7ad
earlgrey02 <san06036@naver.com>
2024-06-16 17:16:47 +0900

    Feat: Chakra Theme 설정

---
+++ src/lib/chakra/theme.ts
@@ -0,0 +1,40 @@
+import { extendTheme, ThemeOverride } from '@chakra-ui/react'
+import styles from '@/lib/chakra/styles'
+import config from '@/lib/chakra/config'
+import fonts from '@/lib/chakra/fonts'
+
+const theme: ThemeOverride = {
+  fonts,
+  styles,
+  config,
+  semanticTokens: {
+    colors: {
+      'grayAlpha.200': {
+        _light: 'blackAlpha.200',
+        _dark: 'whiteAlpha.200'
+      },
+      'grayAlpha.600': {
+        _light: 'blackAlpha.600',
+        _dark: 'whiteAlpha.600'
+      },
+      'grayAlpha.800': {
+        _light: 'blackAlpha.800',
+        _dark: 'whiteAlpha.800'
+      }
+    }
+  },
+  components: {
+    Link: {
+      baseStyle: {
+        transition: '0.3s',
+        _hover: {
+          textDecoration: 'none',
+          transform: 'scale(1.05)',
+          cursor: 'pointer'
+        }
+      }
+    }
+  }
+}
+
+export default extendTheme(theme)
